3e0771042693879aec1ed3d2436345aa1ed34d72
baong <nguyenhiengiabao12@gmail.com>
2025-10-14 13:05:15 +0700

    chore: remove .next from git tracking

--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/node_modules_de08c578._.js",
-  "static/chunks/_5c739eab._.js"
-],
-    source: "dynamic"
-});
